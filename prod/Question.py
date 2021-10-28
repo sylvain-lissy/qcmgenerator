bd72b765d74d1ab00da15138b1ad25d55915d62a
Beefr <coreno@laposte.net>
2021-10-28 13:36:53 +0200

    creation de la classe question

---
+++ prod/Question.py
@@ -0,0 +1,11 @@
+class Question(object):
+
+    def __init__(self, question, reponses, index, themes):
+        self._themes= themes
+        self._index=index
+        self._question=question
+        self._reponses=reponses
+
+    
+
+    
