6427dbfa795ffb65813c58fc4aae517f4ef272ab
Beefr <corentin.renault@telecom-st-etienne.fr>
2021-11-23 11:55:44 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-gunicorn --bind=0.0.0.0:9060 --daemon reload:app
-gunicorn --bind 0.0.0.0:9061 --reload --chdir www app:app
+#gunicorn --bind=0.0.0.0:9060 --daemon reload:app
+#gunicorn --bind 0.0.0.0:9061 --reload --chdir www app:app
